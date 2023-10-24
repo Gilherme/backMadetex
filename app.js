42436b09a4193f49460492b4b595a2fe328a98e7
Gbarretoo <guibarreto64042@outlook.com>
2023-10-24 20:52:29 -0300

    tentando adicionar linha a tabela do mysql rds

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var app = require('./config/server')
 const port = 1039;
 
+let conn = app.config.dbConnection 
+conn.query(`INSERT INTO sua_tabela
+ (chave, param, local) 
+ VALUES ('sua_chave', 'param teste 1', 'seu_local);"`,
+  (err, result) =>{
+  if(err){
+    console.log("deu ruim " + err)
+  }else{
+    console.log('adicionado com sucesso')
+  }
+})
 
 app.listen(port, () => {
   console.log(`servidor on na porta http://localhost:${port}`)
