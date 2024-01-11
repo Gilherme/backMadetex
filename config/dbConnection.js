a3c9ed1344f750aeb80ab96db74933d877a61a33
Gbarretoo <guibarreto64042@outlook.com>
2024-01-11 17:04:09 -0300

    arrumando conecxão com bd

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,18 +1,11 @@
 var mysql = require('mysql')
 
 let conMysql = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: '1234',
+  host: "db-madetex.ccpmwo68291q.us-east-1.rds.amazonaws.com",
+  user: "Guilherme",
+  password: 'NPct9Ua4hL68jez667iX',
   database: 'db_madetex',
 });
-
-// let conMysql = mysql.createConnection({
-//   host: "db-madetex.ccpmwo68291q.us-east-1.rds.amazonaws.com",
-//   user: "Guilherme",
-//   password: 'NPct9Ua4hL68jez667iX',
-//   database: 'db_madetex',
-// });
 
 module.exports = function() {
   return conMysql;
